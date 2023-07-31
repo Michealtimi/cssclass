fd98f38f85ec0395884a5e25da2c616560cddd28
Mike <michealagunbiade@gmail.com>
2023-07-31 04:19:26 -0700

    Rock Udated

---
+++ index.js
@@ -0,0 +1,8 @@
+if (16 >= 18){
+    console.log('you are Eligible')
+    console.log('congratulation')
+}else if(15 >= 16) {
+    console.log('you are almost there')
+}else{
+    console.log('Get out')
+}
